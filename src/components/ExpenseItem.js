fcb34a4601ff1f9c584dcd7c9402b0bda54b8c1d
Shreyaskar <shreyash0790@gmail.com>
2023-12-21 13:55:59 +0530

    using props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+
+function ExpenseItem(props) {
+
+
   return (
     <div className="expense-item">
-      <div>March 21 2023</div>
+    <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>Car insurance</h2>
-        <div className="expense-item__price">$23</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
