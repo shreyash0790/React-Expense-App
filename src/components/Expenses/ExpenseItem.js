15bf688e379de581dff314b733e5a01432186df4
Shreyaskar <shreyash0790@gmail.com>
2023-12-28 13:50:55 +0530

    useSate Cases added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,24 @@
+import React, {useState} from 'react'
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
-  const deleteExp=()=>{
-   const card=document.querySelector(".expense-item")
-   card.remove()
+
+  const [amount, setAmount]= useState(props.amount)
+
+
+
+  const ChangeAmount=()=>{
+   setAmount(100)
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-        <button onClick={deleteExp} className="expense-item__price">Delete</button>
+        <div className="expense-item__price">${amount}</div>
+        <button onClick={ChangeAmount} className="expense-item__price">Change Amount </button>
       </div>
      
     </Card>
