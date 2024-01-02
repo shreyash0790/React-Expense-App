a09d5f94c41f8583779c7e1976658d10de8058ae
Shreyaskar <shreyash0790@gmail.com>
2024-01-02 06:53:34 +0530

    conditioning added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,18 +11,29 @@ function Expenses(prop){
     const filterChangeHandler = selectedYear => {
       setFilteredYear(selectedYear);
     };
+
+const filteredExpenses=expenses.filter((expense)=>{
+  return expense.date.getFullYear().toString()===filteredYear
+})
+
+let expensesContent=<p>No Expenses Found!</p>
+
+if(filteredExpenses.length>0){
+  expensesContent= filteredExpenses.map((expense)=>
+    <ExpenseItem
+    key={expense.id}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+    />
+  )
+}
+
       return (
          
         <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {expenses.map((expense) => 
-          <ExpenseItem
-          key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        )}
+       {expensesContent}
       </Card>
 
         
